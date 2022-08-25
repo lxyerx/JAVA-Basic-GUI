0a2394574ab8e1cb9378e016dbf3fddce10d3710
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-08-25 08:16:37 +0300

    Create ArtıkYılHesaplama.java

---
+++ ArtıkYılHesaplama.java
@@ -0,0 +1,35 @@
+public static void ArtikYilHesaplama(int Yil) {
+
+		boolean artikyil = false;
+		if (Yil % 4 == 0) {
+			if (Yil % 100 == 0) {
+				if (Yil % 400 == 0)
+					artikyil = true;
+				else
+					artikyil = false;
+			} else
+				artikyil = true;
+		} else
+			artikyil = false;
+
+		if (artikyil)
+			JOptionPane.showMessageDialog(null, Yil + " - Artık Yıldır");
+		else
+			JOptionPane.showMessageDialog(null, Yil + " - Artık Yıl Degildir");
+
+	}
+
+	public static void main(String[] args) {
+
+		String Yil = JOptionPane.showInputDialog(null, "Hesaplanacak Yılı giriniz: ", "Artık Yıl Hesaplama",
+				JOptionPane.INFORMATION_MESSAGE);
+
+		try {
+			int YeniYil = Integer.valueOf(Yil);
+			ArtikYilHesaplama(YeniYil);
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Hatalı giris yaptiniz", "Üzgünüm", JOptionPane.ERROR_MESSAGE);
+		}
+
+	}
