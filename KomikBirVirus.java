12e6542981d8f0051923f20b1cbc2c3dcb69824e
lxyerx <95114297+lxyerx@users.noreply.github.com>
2022-08-25 08:19:58 +0300

    Create KomikBirVirus.java
    Kullanıcı arayüzü olan bir şey öğrenince aklıma böyle bir şey geldi komik geldiğinden dolayı yükleme kararı aldım

---
+++ KomikBirVirus.java
@@ -0,0 +1,8 @@
+	public static void main(String[] args) {
+
+		while (true) {
+			JOptionPane.showMessageDialog(null, "Bu Mesaj sadece görev yöneticisinden kapanır", "Hata Mesajı",
+					JOptionPane.ERROR_MESSAGE);
+		}
+
+	}
